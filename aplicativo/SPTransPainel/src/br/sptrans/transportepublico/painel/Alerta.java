7d3391c601d2b6f16f619ec17f87e6dfc77e98da
Rafael Reimberg <rreimberg@gmail.com>
2013-11-02 21:47:28 -0200

    Adicionando projeto da versão 2 do protótipo do Painel

---
+++ aplicativo/SPTransPainel/src/br/sptrans/transportepublico/painel/Alerta.java
@@ -0,0 +1,13 @@
+package br.sptrans.transportepublico.painel;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by rreimberg on 10/31/13.
+ */
+public class Alerta extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
