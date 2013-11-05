a603035920a6d4ba9d872f2e9a04636c0907c619
Rafael Reimberg <rreimberg@gmail.com>
2013-11-05 08:44:52 -0200

    Atualização da barra de progresso com base no ícone clicado

--- aplicativo/SPTransPainel/src/br/sptrans/transportepublico/painel/Lotacao.java
+++ aplicativo/SPTransPainel/src/br/sptrans/transportepublico/painel/Lotacao.java
@@ -1,7 +1,10 @@
 package br.sptrans.transportepublico.painel;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 /**
  * Created by rreimberg on 10/31/13.
@@ -11,5 +14,45 @@ public class Lotacao extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.tela_lotacao);
+
+        findViewById(R.lotacao.nivel1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                atualizaStatus(0);
+            }
+        });
+
+        findViewById(R.lotacao.nivel2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                atualizaStatus(1);
+            }
+        });
+
+        findViewById(R.lotacao.nivel3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                atualizaStatus(2);
+            }
+        });
+
+        findViewById(R.lotacao.nivel4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                atualizaStatus(3);
+            }
+        });
+
+        findViewById(R.lotacao.nivel5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                atualizaStatus(4);
+            }
+        });
+    }
+
+    void atualizaStatus(int status) {
+        ProgressBar progress = (ProgressBar) findViewById(R.lotacao.status);
+        progress.setProgress(status + 1);
     }
 }
