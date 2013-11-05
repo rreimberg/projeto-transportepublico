1b6156de02b284687d6cf7dbf49c608843ddbe58
Rafael Reimberg <rreimberg@gmail.com>
2013-11-05 08:02:35 -0200

    Adicionando EventListeners do Menu Lateral

--- aplicativo/SPTransPainel/src/br/sptrans/transportepublico/painel/MenuFragment.java
+++ aplicativo/SPTransPainel/src/br/sptrans/transportepublico/painel/MenuFragment.java
@@ -1,12 +1,14 @@
 package br.sptrans.transportepublico.painel;
 
 import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentTransaction;
+import android.app.DialogFragment;
 import android.app.ListFragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -15,95 +17,56 @@ import java.util.ArrayList;
 /**
  * Created by rreimberg on 11/3/13.
  */
-public class MenuFragment extends ListFragment {
+public class MenuFragment extends DialogFragment {
 
     boolean mDualPane;
     int mCurCheckPosition = 0;
 
     @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.lateral, container, false);
+        return view;
+    }
+
+    @Override
     public void onActivityCreated(Bundle savedInstanceState) {
-
-        ArrayList titles = new ArrayList();
-        titles.add(0, "Viagem");
-        titles.add(1, "Trânsito");
-        titles.add(2, "Lotação");
-        titles.add(3, "Alertas");
-
         super.onActivityCreated(savedInstanceState);
 
-        // Populate list with our static array of titles.
-        setListAdapter(new ArrayAdapter<String>(getActivity(),
-                android.R.layout.simple_list_item_activated_1, titles));
-
-        // Check to see if we have a frame in which to embed the details
-        // fragment directly in the containing UI.
-        View contentFrame = getActivity().findViewById(R.id.content);
-        mDualPane = contentFrame != null && contentFrame.getVisibility() == View.VISIBLE;
-
-        if (savedInstanceState != null) {
-            // Restore last state for checked position.
-            mCurCheckPosition = savedInstanceState.getInt("curChoice", 0);
-        }
-
-        if (mDualPane) {
-            // In dual-pane mode, the list view highlights the selected item.
-            getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-            // Make sure our UI is in the correct state.
-            showDetails(mCurCheckPosition);
-        }
+        setMenuListeners();
     }
 
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putInt("curChoice", mCurCheckPosition);
-    }
-
-    @Override
-    public void onListItemClick(ListView l, View v, int position, long id) {
-        showDetails(position);
-    }
-
-    /**
-     * Helper function to show the details of a selected item, either by
-     * displaying a fragment in-place in the current UI, or starting a
-     * whole new activity in which it is displayed.
-     */
-    void showDetails(int index) {
-        mCurCheckPosition = index;
-
-        if (mDualPane) {
-            // We can display everything in-place with fragments, so update
-            // the list to highlight the selected item and show the data.
-            getListView().setItemChecked(index, true);
-
-            // Check what fragment is currently shown, replace if needed.
-            ContentFragment details = (ContentFragment)
-                    getFragmentManager().findFragmentById(R.id.content);
-            if (details == null || details.getShownIndex() != index) {
-                // Make new fragment to show this selection.
-                details = ContentFragment.newInstance(index);
-
-                // Execute a transaction, replacing any existing fragment
-                // with this one inside the frame.
-                FragmentTransaction ft = getFragmentManager().beginTransaction();
-                if (index == 0) {
-                    ft.replace(R.id.content, details);
-                } else {
-                    ft.replace(R.id.content, details);
-                }
-                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-                ft.commit();
+    void setMenuListeners() {
+        getActivity().findViewById(R.menu.viagem).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity().getApplicationContext(), Viagem.class);
+                startActivity(i);
             }
+        });
 
-        } else {
-            // Otherwise we need to launch a new activity to display
-            // the dialog fragment with selected text.
-            Intent intent = new Intent();
-            intent.setClass(getActivity(), Viagem.class);
-            intent.putExtra("index", index);
-            startActivity(intent);
-        }
+        getActivity().findViewById(R.menu.transito).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity().getApplicationContext(), Transito.class);
+                startActivity(i);
+            }
+        });
+
+        getActivity().findViewById(R.menu.lotacao).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity().getApplicationContext(), Lotacao.class);
+                startActivity(i);
+            }
+        });
+
+        getActivity().findViewById(R.menu.alerta).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity().getApplicationContext(), Alerta.class);
+                startActivity(i);
+            }
+        });
     }
 
 }
